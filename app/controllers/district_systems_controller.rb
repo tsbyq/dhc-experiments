b18b9e1ecdec4871849e84f39273bfc91ffd1590
Han Li <hanlibpd@gmail.com>
2019-08-21 16:21:36 -0700

    Set up the dispatcher action for the DS controller.

--- app/controllers/district_systems_controller.rb
+++ app/controllers/district_systems_controller.rb
@@ -1,29 +1,66 @@
 class DistrictSystemsController < ApplicationController
-  @district_system_config ={}
+  @district_system_config = {}
 
   def index
     puts 'GET request...'
     # TODO:Get simulation results from session
-    @simulation_results =  {}
+    @simulation_results = {}
     puts @simulation_results
   end
 
   def new
-
+    @district_system = DistrictSystem.new
   end
 
   def create
 
   end
 
-  def simulate
-    puts 'POST request'
+  def dispatcher
+    # TODO: Handle post requests differently depending on their purpose (e.g., upload, visualize, simulate)
+    puts "Do something here!"
+
+    if params[:load_profile_csv]
+      upload_file(params)
+    elsif params[:district_system_config]
+      simulate(params)
+    end
+  end
+
+  def upload_file(params)
+    #TODO: Handle file uploaded, consider saving to database in the future
+    puts 'User uploaded a CSV, visualize it...'
+    puts '*' * 90
+    puts params
+    puts '*' * 90
+
+    uploaded_file = params[:load_profile_csv]
+    puts uploaded_file
+    puts uploaded_file.original_filename
+
+
+    # File.open(Rails.root.join('public', 'uploads', uploaded_file.original_filename), 'wb') do |file|
+    #   puts 'Reading uploaded file...'
+    #   file.write(uploaded_file.read)
+    # end
+
+  end
+
+  def visualize
+    #TODO: Visualize the CSV file use uploaded
+
+  end
+
+  def simulate(params)
+    #TODO: Call EnergyPlus simulation for the district heating and cooling systems
+    puts 'User provided DHC simulation configurations, run it...'
     @simulation_results = {
         a: '123',
         b: '345',
     }
-    @simulation_results = params[:district_system]
-      # render plain: params[:district_system].inspect
+    @simulation_results = params[:district_system_config]
+    # render plain: params[:district_system].inspect
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,12 @@ Rails.application.routes.draw do
 
   get "district_systems", to: "district_systems#index"
 
-  post "district_systems", to: "district_systems#simulate"
+  # TODO: Two post requests on the same view?
+  post "district_systems", to: "district_systems#dispatcher"
+
+  # get "district_systems", to: "district_systems#simulate"
+
+  # post "district_systems", to: "district_systems#simulate"
 
   root to: 'welcome#index'
 end
