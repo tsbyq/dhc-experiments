f00cb3842e64a304ad543b2ed33f8ddd265db5d3
Han Li <hanlibpd@gmail.com>
2019-10-23 14:57:56 -0700

    Added script to modify base plant template idf

--- public/dhc_templates/flexible_template/example/append.py
+++
@@ -1,4 +0,0 @@
-
-
-if __name__ == '__main__':
-    append_files('base.idf', 'plant_loop.idf', 'out.idf')
--- public/dhc_templates/flexible_template/example/idf_processor.py
+++ public/dhc_templates/flexible_template/example/idf_processor.py
@@ -1,38 +1,84 @@
 import eppy
 import copy
+import json
 
+import subprocess
+import os
 from eppy.modeleditor import IDF
 
 
-def modify_template_idf(base_template_idf, modified_template_idf):
-    return
+def modify_template_idf(plant_configuration_json_file,
+                        base_template_idf='base_plant.idf',
+                        modified_template_idf='in.idf',
+                        idd_file = "C:/EnergyPlusV9-1-0/Energy+.idd"):
 
-def expand_template_idf(template_idf, expanded_template_idf):
-    return
+    idf_file = base_template_idf
+    IDF.setiddname(idd_file)
+    idf = IDF(idf_file)
 
-def prepare_LP_plantloop(expanded_plant_loop_idf='expanded.idf', LP_plant_loop_idf='plant_loop.idf'):
+    with open(plant_configuration_json_file) as json_file:
+        data = json.load(json_file)
+
+
+    def add_template_chiller(idf,
+                             chiller_name,
+                             chiller_type='ElectricCentrifugalChiller',
+                             cop=3.5,
+                             condenser_type='WaterCooled'):
+        idf.newidfobject("HVACTemplate:Plant:Chiller")
+        idf_new_template_chiller = idf.idfobjects['HVACTemplate:Plant:Chiller'][-1]
+        idf_new_template_chiller.Name = chiller_name
+        idf_new_template_chiller.Chiller_Type = chiller_type
+        idf_new_template_chiller.Nominal_COP = cop
+        idf_new_template_chiller.Condenser_Type = condenser_type
+        return idf
+
+    def add_template_boiler(idf, boiler_name, efficiency=0.8):
+        idf.newidfobject("HVACTemplate:Plant:Boiler")
+        idf_new_template_boiler = idf.idfobjects['HVACTemplate:Plant:Boiler'][-1]
+        idf_new_template_boiler.Name = boiler_name
+        idf_new_template_boiler.Efficiency = efficiency
+        return idf
+
+    for count, chiller in enumerate(data['chillers']):
+        idf = add_template_chiller(idf, f"Chiller No. {count+1}", chiller['type'], chiller['COP'], chiller['condenser'])
+
+    for count, boiler in enumerate(data['boilers']):
+        idf = add_template_boiler(idf, f"Boiler No. {count+1}", boiler['efficiency'])
+
+    print('---> Adding template chiller and boiler done.')
+    idf.saveas(modified_template_idf)
+
+
+def expand_template_idf(template_idf='in.idf',
+                        expanded_template_idf='expanded.idf',
+                        expand_objects_exe='ExpandObjects.exe',
+                        idd_file = "C:/EnergyPlusV9-1-0/Energy+.idd"):
+    cmd = [expand_objects_exe]
+    p = subprocess.Popen(cmd)
+
+def prepare_LP_plantloop(expanded_plant_loop_idf='expanded.idf',
+                         LP_plant_loop_idf='plant_loop.idf',
+                         idd_file = "C:/EnergyPlusV9-1-0/Energy+.idd"):
     BRANCH_TO_DELETE = ['Test room Cooling Coil ChW Branch', 'Test room Heating Coil HW Branch']
     DEMAND_BRANCH_LISTS = ['Hot Water Loop HW Demand Side Branches', 'Chilled Water Loop ChW Demand Side Branches']
     DEMAND_SPLITTER_LISTS = ['Hot Water Loop HW Demand Splitter', 'Chilled Water Loop ChW Demand Splitter']
     DEMAND_MIXER_LISTS = ['Hot Water Loop HW Demand Mixer', 'Chilled Water Loop ChW Demand Mixer']
-    
+
     OLD_HW_LP_BRANCH_NAME = 'Test room Heating Coil HW Branch'
     OLD_CW_LP_BRANCH_NAME = 'Test room Cooling Coil ChW Branch'
     NEW_HW_LP_BRANCH_NAME = 'Hot Water Loop Load Profile Demand Branch'
     NEW_CW_LP_BRANCH_NAME = 'Chilled Water Loop Load Profile Demand Branch'
-    
-    idd_file = "C:/EnergyPlusV9-1-0/Energy+.idd"
+
     idf_file = expanded_plant_loop_idf
-    epw_file = "in.epw"
-    
     IDF.setiddname(idd_file)
-    idf = IDF(idf_file, epw_file)
-    
+    idf = IDF(idf_file)
+
     branches = idf.idfobjects['Branch']
     branche_lists = idf.idfobjects['BranchList']
     splitter_lists = idf.idfobjects['Connector:Splitter']
     mixer_lists = idf.idfobjects['Connector:Mixer']
-    
+
     # 1. Remove unnecessary objects
     def remove_all_obj_by_type(idf, obj_type_name, exception_name_list=[]):
         if exception_name_list == []:
@@ -41,7 +87,7 @@ def prepare_LP_plantloop(expanded_plant_loop_idf='expanded.idf', LP_plant_loop_i
             old_objcts = idf.idfobjects[obj_type_name]
             idf.idfobjects[obj_type_name] = [obj for obj in old_objcts if obj.Name in exception_name_list]
         return idf
-    
+
     remove_all_obj_by_type(idf, 'Sizing:Parameters')
     remove_all_obj_by_type(idf, 'ScheduleTypeLimits')
     remove_all_obj_by_type(idf, 'Schedule:Compact', ['HVACTemplate-Always 1', 'HVACTemplate-Always 29.4'])
@@ -58,13 +104,13 @@ def prepare_LP_plantloop(expanded_plant_loop_idf='expanded.idf', LP_plant_loop_i
     remove_all_obj_by_type(idf, 'Coil:Cooling:Water')
     remove_all_obj_by_type(idf, 'Coil:Heating:Water')
     remove_all_obj_by_type(idf, 'Output:PreprocessorMessage')
-    
-    
+
+
     # 2. Delete the old zone coil branch objects
     new_branches = [branch for branch in branches if branch.Name not in BRANCH_TO_DELETE]
     idf.idfobjects['Branch'] = new_branches
-    
-    
+
+
     # 3. Rename the demand branches from zone coil branch to load profile branch in the BranchList and  Mixer/Splitter objects
     for branch_list in branche_lists:
         if branch_list.Name in DEMAND_BRANCH_LISTS:
@@ -72,21 +118,21 @@ def prepare_LP_plantloop(expanded_plant_loop_idf='expanded.idf', LP_plant_loop_i
                 branch_list.Branch_2_Name = NEW_HW_LP_BRANCH_NAME
             elif branch_list.Branch_2_Name == OLD_CW_LP_BRANCH_NAME:
                 branch_list.Branch_2_Name = NEW_CW_LP_BRANCH_NAME
-    
+
     for splitter in splitter_lists:
         if splitter.Name in DEMAND_SPLITTER_LISTS:
             if splitter.Outlet_Branch_2_Name == OLD_HW_LP_BRANCH_NAME:
                 splitter.Outlet_Branch_2_Name = NEW_HW_LP_BRANCH_NAME
             if splitter.Outlet_Branch_2_Name == OLD_CW_LP_BRANCH_NAME:
                 splitter.Outlet_Branch_2_Name = NEW_CW_LP_BRANCH_NAME
-    
+
     for mixer in mixer_lists:
         if mixer.Name in DEMAND_MIXER_LISTS:
             if mixer.Inlet_Branch_2_Name == OLD_HW_LP_BRANCH_NAME:
                 mixer.Inlet_Branch_2_Name = NEW_HW_LP_BRANCH_NAME
             if mixer.Inlet_Branch_2_Name == OLD_CW_LP_BRANCH_NAME:
                 mixer.Inlet_Branch_2_Name = NEW_CW_LP_BRANCH_NAME
-    
+
     # 4. Save the idf.
     idf.saveas(LP_plant_loop_idf)
 
@@ -100,7 +146,9 @@ def append_files(file_1, file_2, file_out):
 
 
 if __name__ == '__main__':
-    base_LP_idf = 'base.idf'
+    modify_template_idf('plant_configuration.json')
+    expand_template_idf()
+    base_LP_idf = 'base_LP.idf'
     expanded_plant_loop_idf = 'expanded.idf'
     LP_plant_loop_idf = 'plant_loop.idf'
     final_idf = 'final_999.idf'
