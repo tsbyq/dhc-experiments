d0c94ceea0cde5647c344b82c52e979809ee0ef3
Han Li <hanlibpd@gmail.com>
2019-10-30 13:46:14 -0700

    clean up

--- public/dhc_templates/flexible_template/heat_recovery_template/sys_3_idf_processor.py
+++ public/dhc_templates/flexible_template/heat_recovery_template/sys_3_idf_processor.py
@@ -18,35 +18,17 @@ def modify_template_idf(plant_configuration_json_file,
         data = json.load(json_file)
 
 
-
 idd_file="C:/EnergyPlusV9-1-0/Energy+.idd"
-base_template_idf = "base_CentralHeatPumpSystem.idf"
+base_template_idf = "sys_3_template.idf"
 
 IDF.setiddname(idd_file)
 idf = IDF(base_template_idf)
 
-idf_central_heat_pump_system = idf.idfobjects['CentralHeatPumpSystem'][0]
+NOMINAL_COP = 6.5
+DICT_CHILLER_HEATER = 1
+
+idf.idfobjects['ChillerHeaterPerformance:Electric:EIR'][0].Reference_Cooling_Mode_COP = NOMINAL_COP
+idf.idfobjects['CentralHeatPumpSystem'][0].Number_of_Chiller_Heater_Modules_1 = DICT_CHILLER_HEATER
 
 
-# print(idf_central_heat_pump_system)
-# print(idf.idfobjects['ChillerHeaterPerformance:Electric:EIR'])
-
-base_chiller_heater_eir = idf.idfobjects['ChillerHeaterPerformance:Electric:EIR'][0]
-
-NOMINAL_COP = 3.2
-DICT_CHILLER_HEATER = 5
-
-# for i in range(1, DICT_CHILLER_HEATER+1):
-#     if i == 1:
-#         temp_chiller_boiler_eir = base_chiller_heater_eir
-#     else:
-#         temp_chiller_boiler_eir = idf.copyidfobject(base_chiller_heater_eir)
-#     temp_chiller_boiler_eir.Name = f"Chiller Heater Performance Electric EIR {i}"
-#     temp_chiller_boiler_eir.Reference_Cooling_Mode_COP = NOMINAL_COP
-
-base_chiller_heater_eir.Reference_Cooling_Mode_COP = NOMINAL_COP
-idf_central_heat_pump_system.Number_of_Chiller_Heater_Modules_1 = DICT_CHILLER_HEATER
-
-
-print(idf_central_heat_pump_system)
-print(idf.idfobjects['ChillerHeaterPerformance:Electric:EIR'])
+idf.save("C:/Users/hlee9/Documents/GitHub/dhc-experiments/public/dhc_templates/temp/heat_recovery/sys_3_mutated.idf")
